a801763df5a100a94fe29678817188cf14720492
carlos <carlos.blanco01@correo.usa.edu.co>
2022-09-21 08:30:03 -0500

    Se agrega módulo para usar ejecución de procesos en paralelo

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -1,16 +1,31 @@
 defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
+  def start () do
+    spawn(__MODULE__, :loop, [])
+  end
+
+  def start_link () do
+    spawn_link(__MODULE__, :loop, [])
+  end
+
+  def start_monitor () do
+    spawn_monitor(__MODULE__, :loop, [])
+  end
+
   def loop() do
     receive do
-      {:sequence, n} ->
-        Fibonacci.secuence(n)
-        |> IO.inspect()
+      {:sequence, caller, n} ->
+        result = Fibonacci.secuence(n)
+        send caller, {:fibonacci, n, result, }
         loop()
 
       {:status, msg} ->
         IO.puts("Running #{inspect(msg)}")
         loop()
+
+      {:exit, reason}  ->
+        IO.puts("bye for #{inspect(reason)}")
 
       message ->
         IO.inspect(message)
